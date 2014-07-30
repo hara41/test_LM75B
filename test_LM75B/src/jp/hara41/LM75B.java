d05a7401f9ad2952058dd2c3d12e60a1ad030a9d
Shinichi Hara <hara.shinichi@gmail.com>
2014-07-30 15:50:48 +0900

    first commit

---
+++ test_LM75B/src/jp/hara41/LM75B.java
@@ -0,0 +1,11 @@
+package jp.hara41;
+
+import jp.nyatla.mimic.mbedjs.MbedJsException;
+import jp.nyatla.mimic.mbedjs.javaapi.*;
+
+public class LM75B {
+	public LM75B(Mcu mcu,int sda , int scl ,int address )
+	{
+		
+	}
+}
---
+++ test_LM75B/src/jp/hara41/MainProc.java
@@ -0,0 +1,10 @@
+package jp.hara41;
+
+public class MainProc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
