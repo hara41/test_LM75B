a49e079e5a70d8071b3ec097e541d12ce77430e8
hara41 <hara.shinichi@gmail.com>
2014-08-03 20:27:09 +0900

    first commit

--- test_LM75B/src/jp/hara41/MainProc.java
+++ test_LM75B/src/jp/hara41/MainProc.java
@@ -1,9 +1,19 @@
 package jp.hara41;
-
+import jp.nyatla.mimic.mbedjs.*;
+import jp.nyatla.mimic.mbedjs.javaapi.*;
 public class MainProc {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Mcu mcu;
+		try {
+			mcu = new Mcu("10.0.0.2");
+			LM75B lm = new LM75B(mcu , PinName.p28 , PinName.p27, 0x90);
+			System.out.println(lm.read());
+		} catch (MbedJsException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
